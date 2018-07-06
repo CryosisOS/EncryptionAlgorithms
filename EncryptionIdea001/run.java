13bf55234748dae2473141dfc83afb632ec827dc
Nathan van der Velde <nathan@lakesnetworking.local>
2018-07-06 15:35:34 +0800

    Made a directory
    Signed-off-by: Nathan van der Velde <nathan@lakesnetworking.local>

---
+++ EncryptionIdea001/run.java
@@ -0,0 +1,12 @@
+/**
+ * Author: Nathan van der Velde
+ * Date Created: 2018-07-06
+ */
+
+public class run
+{
+    public static void main(String [] args)
+    {
+
+    }//END main
+}//END class run
